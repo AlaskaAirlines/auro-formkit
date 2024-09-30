2689db33f6c7ae7c534a8785e25e3b3fce1e2773
Jason Baker <jason@capsule42.com>
2024-09-30 11:47:42 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import { AuroForm } from './src/auro-form.js';
+
+/**
+ * Register Custom Element.
+ * @param {Object} name - Name to use for custom element.
+ * @returns {void}
+ */
+ const registerComponent = (name = 'custom-form') => {
+  // alias definition
+  if (!customElements.get(name)) {
+    customElements.define(name, class extends AuroForm {});
+  }
+}
+
+export { registerComponent }
---
+++ test/auro-form.test.js
@@ -0,0 +1,27 @@
+import { fixture, html, expect } from '@open-wc/testing';
+import '../src/auro-form';
+
+describe('auro-form', () => {
+  it('sets the CSS class on auro-form > div element', async () => {
+    const el = await fixture(html`
+      <auro-form cssclass="testClass"></auro-form>
+    `);
+
+    const div = el.shadowRoot.querySelector('div');
+    expect(div.className).to.equal('testClass');
+  });
+
+  it('auro-form is accessible', async () => {
+    const el = await fixture(html`
+      <auro-form cssclass="testClass"></auro-form>
+    `);
+
+    await expect(el).to.be.accessible();
+  });
+
+  it('auro-form custom element is defined', async () => {
+    const el = await !!customElements.get("auro-form");
+
+    await expect(el).to.be.true;
+  });
+});
