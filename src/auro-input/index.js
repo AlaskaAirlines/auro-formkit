266aeb86fc0a98e774e3ff100903dafb678bce6f
Chris Friedberg <Chris.Friedberg@alaskaair.com@SEAHGCFRIED.local>
2024-10-02 15:15:15 -0700

    fix: adding component entry files and missing sass files

---
+++ src/auro-input/index.js
@@ -0,0 +1,4 @@
+import { AuroInput } from './auro-input.js';
+import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
+
+RuntimeUtils.default.prototype.registerComponent('custom-input', AuroInput);
