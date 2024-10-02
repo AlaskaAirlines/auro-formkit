aab5e8baa1b28b958a7f2a3f5e77d6581ae7d7f8
Chris Friedberg <Chris.Friedberg@alaskaair.com@SEAHGCFRIED.local>
2024-10-02 15:15:15 -0700

    fix: adding component entry files and missing sass files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,24 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import serve from 'rollup-plugin-serve';
+import glob from 'glob';
+import path from 'path';
 
-const production = !process.env.ROLLUP_WATCH,
+const production = !process.env.ROLLUP_WATCH;
 
- modernConfig = {
+// Function to get additional entry points
+const getAdditionalEntryPoints = () => {
+  const files = glob.sync('src/*/index.js');
+  return files.reduce((acc, file) => {
+    const name = path.basename(path.dirname(file));
+    acc[`${name}__bundled`] = file;
+    return acc;
+  }, {});
+};
+
+const modernConfig = {
   input: {
     ['auro-form__bundled']: './index.js',
+    ...getAdditionalEntryPoints()
   },
   output: {
     format: 'esm',
---
+++ src/dropdown/index.js
@@ -0,0 +1,4 @@
+import { AuroDropdown } from './auro-dropdown.js';
+import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
+
+RuntimeUtils.default.prototype.registerComponent('custom-dropdown', AuroDropdown);
