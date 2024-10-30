f3c96f75340b1a6e619c26b1338fbe65d769b97c
Chris Friedberg <chris.friedberg@alaskaair.com>
2024-10-30 15:23:52 -0700

    fix: do not reassign parameter

--- components/dropdown/demo/index.js
+++ components/dropdown/demo/index.js
@@ -1,20 +1,14 @@
 /* eslint-disable jsdoc/require-jsdoc, no-magic-numbers, no-param-reassign */
-
-// import { auroMenuResetExample } from '../apiExamples/reset';
-
-export function initExamples(initCount) {
-  initCount = initCount || 0;
-
+export function initExamples(initialCount = 0) {
   try {
     // javascript example function calls to be added here upon creation to test examples
     // auroMenuResetExample();
   } catch (err) {
-    if (initCount <= 20) {
+    if (initialCount <= 20) {
       // setTimeout handles issue where content is sometimes loaded after the functions get called
       setTimeout(() => {
-        initExamples(initCount + 1);
+        initExamples(initialCount + 1);
       }, 100);
     }
   }
 }
-
