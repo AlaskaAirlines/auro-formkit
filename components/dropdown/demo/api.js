774e0f8d8958d71e4d53e9dc673b92c2ae20568b
Chris Friedberg <chris.friedberg@alaskaair.com>
2024-10-30 13:08:30 -0700

    fix: do not reassign parameter

--- components/dropdown/demo/api.js
+++ components/dropdown/demo/api.js
@@ -1,22 +1,18 @@
-/* eslint-disable jsdoc/require-jsdoc, no-magic-numbers, no-param-reassign */
-
+/* eslint-disable jsdoc/require-jsdoc, no-magic-numbers */
 import { showExample } from './../apiExamples/programmaticallyShow.js';
 import { hideExample } from './../apiExamples/programmaticallyHide.js';
 
-export function initExamples(initCount) {
-  initCount = initCount || 0;
-
+export function initExamples(initialCount = 0) {
   try {
     // javascript example function calls to be added here upon creation to test examples
     showExample();
     hideExample();
   } catch (err) {
-    if (initCount <= 20) {
+    if (initialCount <= 20) {
       // setTimeout handles issue where content is sometimes loaded after the functions get called
       setTimeout(() => {
-        initExamples(initCount + 1);
+        initExamples(initialCount + 1);
       }, 100);
     }
   }
 }
-
