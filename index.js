2af691187a6ea63b89b5cf506517c21ac82911e5
Chris Friedberg <chris.friedberg@alaskaair.com>
2024-11-04 15:39:47 -0800

    fix: adding auro-form entry files

---
+++ index.js
@@ -0,0 +1,4 @@
+import { AuroForm } from './auro-form.js';
+import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
+
+RuntimeUtils.default.prototype.registerComponent('custom-form', AuroForm);
