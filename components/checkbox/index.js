7318c4176fb3508446e782d2ffaf03b8fd2541de
Eunsun Mota <eunsun.mota@alaskaair.com>
2024-11-05 19:09:33 -0500

    feat: add checkbox into components #27

---
+++ components/checkbox/index.js
@@ -0,0 +1,6 @@
+import { AuroCheckbox } from './src/auro-checkbox.js';
+import { AuroCheckboxGroup } from './src/auro-checkbox-group.js';
+
+
+AuroCheckbox.register();
+AuroCheckboxGroup.register();
