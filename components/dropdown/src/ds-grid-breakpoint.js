a01e883f06358e413095c9bc739a6d98d7d7654b
Eunsun Mota <eunsun.mota@alaskaair.com>
2024-11-27 16:00:22 -0500

    ci: add a script to extract breakpoint value in js

---
+++ components/dropdown/src/ds-grid-breakpoint.js
@@ -0,0 +1,4 @@
+/* eslint-disable */
+export default {
+'ds-grid-breakpoint-xs': '320px','ds-grid-breakpoint-sm': '576px','ds-grid-breakpoint-md': '768px','ds-grid-breakpoint-lg': '1024px','ds-grid-breakpoint-xl': '1232px'
+};
