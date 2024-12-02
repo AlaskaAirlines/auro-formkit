76a770d6817b34f84d14ea22c6caa37031d89c41
chrisfalaska <>
2024-12-02 14:56:44 -0800

    fix: remove date-picker vendor files from root

--- components/datepicker/vendor/wc-range-datepicker/day.js
+++
@@ -1,8 +0,0 @@
-import { format } from 'date-fns';
-export class Day {
-    constructor(date) {
-        this.date = parseInt(format(date, 't'), 10);
-        this.title = parseInt(format(date, 'd'), 10);
-    }
-}
-//# sourceMappingURL=day.js.map
