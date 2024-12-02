155e3dfa07009d970e9a9c75fcb6d79a40dcd961
chrisfalaska <>
2024-12-02 12:48:04 -0800

    build: update versionWriter package paths

--- components/combobox/src/scripts/version.mjs
+++ components/combobox/src/scripts/version.mjs
@@ -1,4 +1,4 @@
-import { writeDepVersionFile } from '../../../../scripts/formVersionWriter.js'
+import { writeDepVersionFile } from '@auro-formkit/build-tools/formVersionWriter';
 
 writeDepVersionFile('@/components/dropdown');
 writeDepVersionFile('@/components/input');
