343a69b240fac53d61e15366cf530ffe08a9d7a9
chrisfalaska <>
2024-12-03 11:40:53 -0800

    fix: update component dependencies to workspaces

--- components/select/test/auro-select.test.js
+++ components/select/test/auro-select.test.js
@@ -1,5 +1,5 @@
 import { fixture, html, expect, waitUntil, elementUpdated } from '@open-wc/testing';
-import '@aurodesignsystem/auro-dropdown';
+import '@auro-formkit/auro-dropdown';
 import '../../menu/src/index.js';
 import '../src/index.js';
 
