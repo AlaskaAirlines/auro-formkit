18d5103e617e2554f9f1df21e367fbae8c3b4f5c
chrisfalaska <>
2024-12-03 10:56:22 -0800

    fix: export Dropdown & Import so Combobox can consume

--- components/dropdown/src/index.js
+++ components/dropdown/src/index.js
@@ -1,3 +1,6 @@
 import { AuroDropdown } from './auro-dropdown.js';
 
 AuroDropdown.register();
+
+// export so other components can import the component
+export { AuroDropdown };
--- components/input/src/index.js
+++ components/input/src/index.js
@@ -1,3 +1,6 @@
 import { AuroInput } from './auro-input.js';
 
 AuroInput.register();
+
+// export so other components can import the component
+export { AuroInput };
