282add70e2729a0f625c24673d5cda7befeef1b5
chrisfalaska <>
2024-12-04 12:17:37 -0800

    chore: update js comments

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,7 @@
-// commitlint.config.js needs to be at the root
-// where husky is installed and running
+/*
+  commitlint.config.js needs to be at the root
+  where Husky is installed and running
+*/
 import config from '@auro-formkit/config/commitlint';
+
 export default config;
