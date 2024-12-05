f3c5fb25cca66a73ae9bc3f181a56f5aad8ff06a
rmenner <ryan.menner@alaskaair.com>
2024-12-04 19:01:13 -0500

    chore: change dropdown ref to counter

--- components/counter/demo/index.js
+++ components/counter/demo/index.js
@@ -1,11 +1,9 @@
 /* eslint-disable jsdoc/require-jsdoc, no-magic-numbers, no-param-reassign */
-// import { AuroDropdownBib } from '../src/auro-dropdownBib.js';
 import { AuroCounter } from '../src/auro-counter.js';
 
-// AuroDropdownBib.register();
 
 AuroCounter.register();
-AuroCounter.register('custom-dropdown');
+AuroCounter.register('custom-counter');
 
 export function initExamples(initialCount = 0) {
   try {
