2bb2986056bd0c0e9051ee71a6636a8eeae286c7
Eunsun Mota <eunsun.mota@alaskaair.com>
2024-12-16 18:37:01 -0500

    feat: add loading-placeholder in menu

---
+++ components/select/apiExamples/loading.js
@@ -0,0 +1,32 @@
+export function auroMenuLoadingExample() {
+  const select = document.querySelector("#loadingExample");
+  const menu = document.querySelector("#loadingExampleSelectMenu");
+  const emptyMenu = () => {
+
+    const menuoptions = menu.children;
+    for (let i = menuoptions.length - 1; i >= 0; i--) {
+      if (menuoptions[i].value) {
+        menu.removeChild(menuoptions[i]);
+      }
+    }
+  }
+  const fillMenu = () => {
+      menu.innerHTML += `
+      <auro-menuoption value="stops">Stops</auro-menuoption>
+      <auro-menuoption value="price">Price</auro-menuoption>
+      <auro-menuoption value="duration">Duration</auro-menuoption>
+      <auro-menuoption value="departure">Departure</auro-menuoption>
+      <auro-menuoption value="arrival">Arrival</auro-menuoption>
+      <auro-menuoption value="prefer alaska">Prefer Alaska</auro-menuoption>`;
+  }
+  select.addEventListener("click", () => {
+    if (!menu.hasAttribute('loading')) {
+      emptyMenu();
+      menu.setAttribute('loading', 'loading');
+      setTimeout(() => {
+        menu.removeAttribute('loading');
+        fillMenu();
+      }, 1000);
+    }
+  });
+}
