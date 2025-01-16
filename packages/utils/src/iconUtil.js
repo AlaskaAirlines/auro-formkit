4f5d55f5959d302c6ac48efdee0f33fa5909a34c
rmenner <ryan.menner@alaskaair.com>
2025-01-16 17:29:41 -0500

    feat: add IconUtil for SVG icon generation #91

---
+++ packages/utils/src/iconUtil.js
@@ -0,0 +1,25 @@
+// Copyright (c) Alaska Air. All right reserved. Licensed under the Apache-2.0 license
+// See LICENSE in the project root for license information.
+
+// ---------------------------------------------------------------------
+
+import { html } from "lit/static-html.js";
+
+export class IconUtil {
+
+  /**
+   * Generates an HTML element containing an SVG icon based on the provided `svgContent`.
+   *
+   * @param {string} svgContent - The SVG content to be embedded.
+   * @param {Object} _attributes - Additional attributes to set on the icon element.
+   * @returns {Element} The HTML element containing the SVG icon.
+   */
+  static generateSvgHtml(svgContent) {
+    const dom = new DOMParser().parseFromString(svgContent.svg, 'text/html');
+    const svg = dom.body.firstChild;
+    
+    svg.setAttribute('slot', 'svg');
+
+    return html`${svg}`;
+  }
+}
