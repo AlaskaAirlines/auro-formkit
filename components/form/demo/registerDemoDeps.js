02ce3d22cc7ba932d84202d033c789b7196d44a0
Eunsun Mota <eunsun.mota@alaskaair.com>
2025-01-24 12:20:17 -0500

    feat: add wide and narrow mode

--- components/form/demo/registerDemoDeps.js
+++ components/form/demo/registerDemoDeps.js
@@ -1,12 +1,14 @@
 import {AuroInput} from "@aurodesignsystem/auro-input";
 import {AuroDatePicker} from "@aurodesignsystem/auro-datepicker";
 import {AuroCombobox} from "@aurodesignsystem/auro-combobox";
+import {AuroSelect} from "@aurodesignsystem/auro-select";
 import {AuroMenu} from "@aurodesignsystem/auro-menu";
 import {AuroCounterGroup, AuroCounter} from "@aurodesignsystem/auro-counter";
 
 AuroInput.register();
 AuroInput.register('auro-input-two');
 AuroDatePicker.register();
+AuroSelect.register();
 AuroCombobox.register();
 AuroMenu.register();
 AuroCounter.register();
