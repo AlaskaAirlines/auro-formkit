5a4b26e194b9aef6868506350999d21ae7132e0b
Doug <doug@dougflynn.dev>
2025-01-28 15:59:09 -0800

    test: more auro form conditions
    Mainly validity and input handling.

--- components/form/test/auro-form.test.js
+++ components/form/test/auro-form.test.js
@@ -1,4 +1,4 @@
-import { fixture, html, expect } from '@open-wc/testing';
+import {fixture, html, expect, elementUpdated} from '@open-wc/testing';
 
 // !AURO ELEMENT REGISTRATION MUST BE DONE BEFORE AURO FORM REGISTRATION! //
 import '../demo/registerDemoDeps.js';
@@ -53,8 +53,8 @@ describe('auro-form', () => {
     `);
 
     await el.updateComplete;
-    await expect(JSON.stringify(el.formState)).to.equal(JSON.stringify(defaultFormState));
-    await expect(JSON.stringify(el.value)).to.equal(JSON.stringify(defaultFormValue));
+    await expect(el.formState).to.deep.equal(defaultFormState);
+    await expect(el.value).to.deep.equal(defaultFormValue);
     await expect(el.validity).to.equal(null);
   });
 
@@ -78,8 +78,122 @@ describe('auro-form', () => {
     `);
 
     await el.updateComplete;
-    await expect(JSON.stringify(el.formState)).to.equal(JSON.stringify(expectedFormState));
-    await expect(JSON.stringify(el.value)).to.equal(JSON.stringify(expectedFormValue));
+    await expect(el.formState).to.deep.equal(expectedFormState);
+    await expect(el.value).to.deep.equal(expectedFormValue);
     await expect(el.validity).to.equal("valid");
+  });
+
+  it('updates values when input events are emitted by form elements', async () => {
+    const expectedFormState = {
+      testInput: {
+        value: "zzz",
+        validity: "valid",
+        required: true
+      }
+    };
+
+    const expectedFormValue = {
+      testInput: "zzz"
+    };
+
+    const el = await fixture(html`
+      <auro-form>
+        <auro-input name="testInput" value="some-preset-value" required></auro-input>
+      </auro-form>
+    `);
+
+    // INTERNAL STATE KEY - FOR TESTING ONLY :)
+    // eslint-disable-next-line no-underscore-dangle
+    const [inputEl] = el._elements;
+
+    inputEl.focus();
+    inputEl.value = 'zzz';
+    inputEl.blur();
+
+    await elementUpdated(el);
+    await expect(el.formState).to.deep.equal(expectedFormState);
+    await expect(el.value).to.deep.equal(expectedFormValue);
+    await expect(el.validity).to.equal("valid");
+  });
+
+  it('updates validity when events are submitted by form elements', async () => {
+    const el = await fixture(html`
+      <auro-form>
+        <auro-input name="testInput" required></auro-input>
+      </auro-form>
+    `);
+
+    await expect(el.formState).to.have.key('testInput');
+    await expect(el.formState.testInput.validity).to.be.null;
+    await expect(el.validity).to.be.null;
+
+    // INTERNAL STATE KEY - used externally for testing only!
+    // eslint-disable-next-line no-underscore-dangle
+    const [inputEl] = el._elements;
+
+    inputEl.focus();
+    inputEl.value = 'zzz';
+    inputEl.blur();
+
+    await elementUpdated(el);
+
+    await expect(el.formState.testInput.validity).to.equal('valid');
+    await expect(el.validity).to.equal('valid');
+  });
+
+  describe('when given multiple inputs', () => {
+    it('has INVALID state when two or more required inputs are present, and only one is populated', async () => {
+      const el = await fixture(html`
+      <auro-form>
+        <auro-input name="testInput" required></auro-input>
+        <auro-input name="testInput2" required></auro-input>
+      </auro-form>
+    `);
+
+      await expect(el.formState).to.have.keys('testInput', 'testInput2');
+      await expect(el.formState.testInput.validity).to.be.null;
+      await expect(el.formState.testInput2.validity).to.be.null;
+      await expect(el.validity).to.be.null;
+
+      // INTERNAL STATE KEY - used externally for testing only!
+      // eslint-disable-next-line no-underscore-dangle
+      const [inputEl] = el._elements;
+
+      inputEl.focus();
+      inputEl.value = 'zzz';
+      inputEl.blur();
+
+      await elementUpdated(el);
+
+      await expect(el.formState.testInput.validity).to.equal('valid');
+      await expect(el.validity).to.equal('invalid');
+    });
+
+    it('has VALID state when two or more inputs are present, and only one is required', async () => {
+      const el = await fixture(html`
+      <auro-form>
+        <auro-input name="testInput" required></auro-input>
+        <auro-input name="testInput2"></auro-input>
+      </auro-form>
+    `);
+
+      await expect(el.formState).to.have.keys('testInput', 'testInput2');
+      await expect(el.formState.testInput.validity).to.be.null;
+      await expect(el.formState.testInput2.validity).to.be.null;
+      await expect(el.validity).to.be.null;
+
+      // INTERNAL STATE KEY - used externally for testing only!
+      // eslint-disable-next-line no-underscore-dangle
+      const [inputEl] = el._elements;
+
+      inputEl.focus();
+      inputEl.value = 'zzz';
+      inputEl.blur();
+
+      await elementUpdated(el);
+
+      await expect(el.formState.testInput.validity).to.equal('valid');
+      await expect(el.validity).to.equal('valid');
+    });
   });
 });
