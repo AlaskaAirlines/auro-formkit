ff80bc0a1e6b218e828163b08b85784250adb7fb
chrisfalaska <>
2025-01-29 11:51:29 -0800

    fix: update config index with exports #283

---
+++ packages/config/src/index.js
@@ -0,0 +1,5 @@
+export { default as commitlint } from './commitlint.config.js';
+export { default as eslint } from './eslint.config.mjs';
+export { default as rollup } from './rollup.config.mjs';
+export { default as stylelint } from './stylelintrc.config.json' assert { type: 'json' };
+export { default as wtr } from './web-test-runner.config.mjs';
