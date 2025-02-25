fb67be4ac218ee66268573fb5271e6ed62b7b360
Eunsun Mota <eunsun.mota@alaskaair.com>
2025-02-25 12:53:04 -0500

    build: remove `@aurodesignsystem/auro-header` from datepicker package

--- components/datepicker/scripts/version.mjs
+++ components/datepicker/scripts/version.mjs
@@ -2,7 +2,6 @@ import { writeDepVersionFile } from '@auro-formkit/build-tools/formVersionWriter
 
 await writeDepVersionFile('@aurodesignsystem/auro-popover');
 await writeDepVersionFile('@aurodesignsystem/auro-dropdown');
-await writeDepVersionFile('@aurodesignsystem/auro-header');
 await writeDepVersionFile('@aurodesignsystem/auro-input');
 await writeDepVersionFile('@aurodesignsystem/auro-bibtemplate');
 
