63b8dac663377a4af4038ed2c5e09e34bf155409
Eunsun Mota <eunsun.mota@alaskaair.com>
2025-05-09 11:12:36 -0400

    fix: render match word in bold in a combobox with dynamic menu #551

--- components/combobox/src/auro-combobox.js
+++ components/combobox/src/auro-combobox.js
@@ -351,6 +351,14 @@ export class AuroCombobox extends LitElement {
    * @returns {void}
    */
   handleMenuOptions() {
+    // Reset menu matchword UI
+    this.menu.updateItemsState(new Map([
+      [
+        'matchWord',
+        true
+      ]
+    ]));
+
     this.generateOptionsArray();
     this.availableOptions = [];
 
