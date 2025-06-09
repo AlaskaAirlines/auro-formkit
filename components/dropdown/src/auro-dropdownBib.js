f8eeccb67c9ce3bca5b1726e223a4a32e4a2675e
Jason Baker <jason@capsule42.com>
2025-06-09 10:30:46 -0700

    refactor: clean up lint issues with dropdown

--- components/dropdown/src/auro-dropdownBib.js
+++ components/dropdown/src/auro-dropdownBib.js
@@ -10,8 +10,8 @@ import { LitElement } from "lit";
 
 import AuroLibraryRuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-import styleCss from "./styles/default/bibStyles-css.js";
-import colorCss from "./styles/default/bibColors-css.js";
+import styleCss from "./styles/classic/bibStyles-css.js";
+import colorCss from "./styles/classic/bibColors-css.js";
 import tokensCss from "./styles/tokens-css.js";
 
 const DESIGN_TOKEN_BREAKPOINT_PREFIX = '--ds-grid-breakpoint-';
