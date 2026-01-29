36ddac2061fc7aff020ac796b3a8e0aaa2d5acd2
Jordan Jones <jmjones2463@gmail.com>
2026-01-29 11:51:54 -0800

    perf: update example js processing to support readme example

--- components/dropdown/demo/api.js
+++ components/dropdown/demo/api.js
@@ -10,24 +10,15 @@ import { showExample } from './../apiExamples/programmatically-show';
 import { inDialogExample } from './../apiExamples/in-dialog';
 import '../src/registered.js';
 
-export function initExamples(initialCount = 0) {
-  try {
-    // javascript example function calls to be added here upon creation to test examples
-    errorExample();
-    inverseErrorExample();
-    fullscreenBreakpointExample();
-    classicExample();
-    classicInverseExample();
-    matchWidthExample();
-    hideExample();
-    showExample();
-    inDialogExample();
-  } catch (err) {
-    if (initialCount <= 20) {
-      // setTimeout handles issue where content is sometimes loaded after the functions get called
-      setTimeout(() => {
-        initExamples(initialCount + 1);
-      }, 100);
-    }
-  }
+export function initExamples() {
+  // javascript example function calls to be added here upon creation to test examples
+  errorExample();
+  inverseErrorExample();
+  fullscreenBreakpointExample();
+  classicExample();
+  classicInverseExample();
+  matchWidthExample();
+  hideExample();
+  showExample();
+  inDialogExample();
 }
