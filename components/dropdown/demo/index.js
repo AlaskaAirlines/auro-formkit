b8e5be4fa8e73a5eb608cf2b8f6b44b1c9333e09
Jordan Jones <jmjones2463@gmail.com>
2026-01-29 11:51:54 -0800

    perf: update example js processing to support readme example

--- components/dropdown/demo/index.js
+++ components/dropdown/demo/index.js
@@ -2,23 +2,15 @@
 
 import { classicExample } from '../apiExamples/classic/basic.js';
 import { classicInverseExample } from '../apiExamples/classic/appearance-inverse.js';
+import { customExample } from '../apiExamples/custom.js';
 
 import { AuroDropdown } from '../src/auro-dropdown.js';
 AuroDropdown.register();
 AuroDropdown.register('custom-dropdown');
 
-export function initExamples(initialCount = 0) {
-  try {
-    // javascript example function calls to be added here upon creation to test examples
-    classicExample();
-    classicInverseExample();
-    customExample();
-  } catch (err) {
-    if (initialCount <= 20) {
-      // setTimeout handles issue where content is sometimes loaded after the functions get called
-      setTimeout(() => {
-        initExamples(initialCount + 1);
-      }, 100);
-    }
-  }
+export function initExamples() {
+  // javascript example function calls to be added here upon creation to test examples
+  classicExample();
+  classicInverseExample();
+  customExample();
 }
