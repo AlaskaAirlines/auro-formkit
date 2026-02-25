7eebbcb6b3407f4514fb6a31d4feae2c6852b8b2
rmenner <rmenner@users.noreply.github.com>
2026-02-25 21:25:30 +0000

    Deploy preview for PR 1353 🛫

---
+++ pr-preview/pr-1353/checkbox/index.js
@@ -0,0 +1,8 @@
+import { AuroCheckbox } from '../src/auro-checkbox.js';
+import { AuroCheckboxGroup } from '../src/auro-checkbox-group.js';
+
+AuroCheckbox.register(); // registering to `auro-checkbox`
+AuroCheckboxGroup.register(); // registering to `auro-checkbox-group`
+
+AuroCheckbox.register('custom-checkbox');
+AuroCheckboxGroup.register('custom-checkbox-group');
