ac90217dec7c2a83edf60bf3f7d4d985d9c21c98
rmenner <rmenner@users.noreply.github.com>
2026-02-27 17:26:57 +0000

    Remove preview for PR 1351 🛬

--- pr-preview/pr-1351/select/index.js
+++
@@ -1,9 +0,0 @@
-// import { exampleFunction } from "exampleFile.js";
-
-/* eslint-disable jsdoc/require-jsdoc, no-magic-numbers, no-param-reassign */
-
-import { AuroSelect } from '../src/auro-select.js';
-import '../../menu/src/registered.js';
-
-AuroSelect.register(); // registering to `auro-select`
-AuroSelect.register('custom-select');
