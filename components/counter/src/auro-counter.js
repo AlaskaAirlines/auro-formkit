7ee64b82687c87693d6c1cddaa7db859c2bf5af5
Chris Friedberg <chris.friedberg@alaskaair.com>
2026-03-09 09:44:14 -0700

    fix: move shadowRootOptions from constructor mutation to static class field

--- components/counter/src/auro-counter.js
+++ components/counter/src/auro-counter.js
@@ -37,6 +37,13 @@ import formkitVersion from '@aurodesignsystem/version';
  * @slot description - Descriptive content for the counter.
  */
 export class AuroCounter extends LitElement {
+  static get shadowRootOptions() {
+    return {
+      ...LitElement.shadowRootOptions,
+      delegatesFocus: true,
+    };
+  }
+
   constructor() {
     super();
 
@@ -83,14 +90,6 @@ export class AuroCounter extends LitElement {
      */
     this.runtimeUtils = new AuroLibraryRuntimeUtils();
 
-    /**
-     * @private
-     * @property {boolean} delegatesFocus - Whether the shadow root delegates focus.
-     */
-    this.constructor.shadowRootOptions = {
-      ...LitElement.shadowRootOptions,
-      delegatesFocus: true,
-    };
   }
 
   connectedCallback() {
