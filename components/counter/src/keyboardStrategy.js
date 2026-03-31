211621c542b3cc7f544ab7e39c65f8302b1e3a4d
Jason Baker <jason@capsule42.com>
2026-03-31 14:09:52 -0700

    refactor: add keyboard strategy for counter

---
+++ components/counter/src/keyboardStrategy.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2026 Alaska Airlines. All right reserved. Licensed under the Apache-2.0 license
+// See LICENSE in the project root for license information.
+
+export const keyboardStrategy = {
+  ArrowUp(component, _evt) {
+    if (component.disabled) {
+      return;
+    }
+
+    _evt.preventDefault();
+    component.increment();
+  },
+
+  ArrowDown(component, _evt) {
+    if (component.disabled) {
+      return;
+    }
+
+    _evt.preventDefault();
+    component.decrement();
+  }
+};
