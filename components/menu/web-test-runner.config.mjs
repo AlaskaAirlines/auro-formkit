6b9d82d059a1b7bce496b7c0c0594b7ab9f61aa6
Jason Baker <jason@capsule42.com>
2026-04-22 08:48:02 -0700

    test: fix timeout issue with menu tests

--- components/menu/web-test-runner.config.mjs
+++ components/menu/web-test-runner.config.mjs
@@ -3,4 +3,9 @@ import baseConfig from '../../packages/config/src/web-test-runner.config.mjs';
 export default {
   ...baseConfig,
   concurrency: 1,
+  testFramework: {
+    config: {
+      timeout: 10000,
+    },
+  },
 };
