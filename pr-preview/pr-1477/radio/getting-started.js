5804c567344a186fae7222f70b7f4e29468ef8bd
jordanjones243 <jordanjones243@users.noreply.github.com>
2026-05-18 22:07:00 +0000

    Deploy preview for PR 1477 🛫

---
+++ pr-preview/pr-1477/radio/getting-started.js
@@ -0,0 +1,24 @@
+import { AuroRadio } from '../src/auro-radio.js';
+import { AuroRadioGroup } from '../src/auro-radio-group.js';
+
+import { resetStateExample } from '../apiExamples/reset-state.js';
+
+AuroRadio.register();
+AuroRadioGroup.register();
+
+AuroRadio.register('custom-radio');
+AuroRadioGroup.register('custom-radio-group');
+
+export function initExamples(initCount) {
+  initCount = initCount || 0;
+
+  try {
+    await resetStateExample();
+  } catch (err) {
+    if (initCount <= 20) {
+      setTimeout(() => {
+        initExamples(initCount + 1);
+      }, 100);
+    }
+  }
+}
