c235185433c2e96dbb0fc73e3e976bb6f2e494c9
Jason Baker <jason@capsule42.com>
2026-05-29 13:04:45 -0700

    test: fix token import links

--- packages/config/src/web-test-runner.config.mjs
+++ packages/config/src/web-test-runner.config.mjs
@@ -34,8 +34,8 @@ export default {
   },
   testRunnerHtml: (testFramework) => `<html>
       <head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@aurodesignsystem/design-tokens@latest/dist/auro-classic/CSSCustomProperties.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@aurodesignsystem/design-tokens@latest/dist/alaska/CSSCustomProperties--alaska.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@aurodesignsystem/design-tokens@latest/dist/legacy/auro-classic/CSSCustomProperties.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@aurodesignsystem/design-tokens@latest/dist/web/alaska.css">
       </head>
       <body>
         <script type="module" src="${testFramework}"></script>
