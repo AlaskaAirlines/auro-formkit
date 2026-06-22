c1c955504397ad388febfb9efe10f01b4590823f
jordanjones243 <jordanjones243@users.noreply.github.com>
2026-06-22 16:16:12 +0000

    Deploy preview for PR 1478 🛫

--- pr-preview/pr-1478/form/customize.js
+++ pr-preview/pr-1478/form/customize.js
@@ -1,9 +1,28 @@
+/* eslint-disable jsdoc/require-jsdoc */
+
 import { AuroForm } from '../src/auro-form.js';
 import './registerDemoDeps.js';
 import { AuroInput } from '@aurodesignsystem/auro-input';
 import { AuroDatePicker } from '@aurodesignsystem/auro-datepicker';
+import { disabledExample } from '../apiExamples/disabled.js';
+import { disableAfterEditExample } from '../apiExamples/disabled-after-edit.js';
 
 AuroInput.register('input-two');
 AuroDatePicker.register();
 AuroForm.register();
 AuroForm.register('custom-form');
+
+export async function initExamples(initCount) {
+  initCount = initCount || 0;
+
+  try {
+    await disabledExample();
+    await disableAfterEditExample();
+  } catch (err) {
+    if (initCount <= 20) {
+      setTimeout(() => {
+        initExamples(initCount + 1);
+      }, 100);
+    }
+  }
+}
