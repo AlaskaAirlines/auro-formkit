d80f6c2774f526d26a95c3aa6a79d0e7d5d0b479
jason-capsule42 <jason-capsule42@users.noreply.github.com>
2026-06-22 18:11:19 +0000

    Remove preview for PR 1507 🛬

--- pr-preview/pr-1507/form/customize.js
+++
@@ -1,9 +0,0 @@
-import { AuroForm } from '../src/auro-form.js';
-import './registerDemoDeps.js';
-import { AuroInput } from '@aurodesignsystem/auro-input';
-import { AuroDatePicker } from '@aurodesignsystem/auro-datepicker';
-
-AuroInput.register('input-two');
-AuroDatePicker.register();
-AuroForm.register();
-AuroForm.register('custom-form');
