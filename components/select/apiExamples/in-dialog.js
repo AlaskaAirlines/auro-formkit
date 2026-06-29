60c68aa75e9481f4ac1eb6fe30d42a04c5b046d2
Chris Friedberg <chris.friedberg@alaskaair.com>
2026-06-29 11:00:06 -0700

    docs(select): remove orphaned apiExamples

--- components/select/apiExamples/in-dialog.js
+++
@@ -1,6 +0,0 @@
-export function inDialogExample() {
-  document.querySelector("#select-dialog-opener").addEventListener("click", () => {
-    const dialog = document.querySelector("#select-dialog");
-    dialog.open = true;
-  });
-};
