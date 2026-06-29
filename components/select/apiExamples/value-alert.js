d0f1835d3617dc5df090ed10d827573d97eca687
Chris Friedberg <chris.friedberg@alaskaair.com>
2026-06-29 11:00:06 -0700

    docs(select): remove orphaned apiExamples

--- components/select/apiExamples/value-alert.js
+++
@@ -1,9 +0,0 @@
-export function valueAlertExample() {
-  const select = document.querySelector('#valueAlert');
-  const menu = document.querySelector('#valueAlertMenu');
-
-  menu.addEventListener('auroMenu-selectedOption', () => {
-    console.warn('Select value changed to:', select.value);
-    console.warn('Select optionSelected changed to:', select.optionSelected);
-  });
-}
