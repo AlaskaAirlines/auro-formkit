1ca542773cb13faf327eaef05fc629830407d3ea
jason-capsule42 <jason-capsule42@users.noreply.github.com>
2026-07-02 22:35:46 +0000

    Remove preview for PR 1530 🛬

--- pr-preview/pr-1530/radio/index.js
+++
@@ -1,8 +0,0 @@
-import { AuroRadio } from '../src/auro-radio.js';
-import { AuroRadioGroup } from '../src/auro-radio-group.js';
-
-AuroRadio.register();
-AuroRadioGroup.register();
-
-AuroRadio.register('custom-radio');
-AuroRadioGroup.register('custom-radio-group');
