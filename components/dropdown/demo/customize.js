a913a524940993c2679091eae809c44a7dfa9b50
Jordan Jones <jmjones2463@gmail.com>
2026-08-04 14:45:06 -0700

    docs(js): fix broken functional js examples AB#1611713

--- components/dropdown/demo/customize.js
+++ components/dropdown/demo/customize.js
@@ -5,12 +5,18 @@ AuroDropdown.register();
 AuroDropdown.register('custom-dropdown');
 
 import { fullscreenBreakpointExample } from '../apiExamples/fullscreen-breakpoint.js';
+import { errorExample } from '../apiExamples/error.js';
+import { matchWidthExample } from '../apiExamples/match-width.js';
+import { inverseErrorExample } from '../apiExamples/appearance-inverse-error.js';
 export function initExamples(initCount) {
   // javascript example function calls to be added here upon creation to test examples
   initCount = initCount || 0;
 
   try {
     fullscreenBreakpointExample();
+    errorExample();
+    matchWidthExample();
+    inverseErrorExample();
   } catch {
     if (initCount <= 20) {
       setTimeout(() => {
