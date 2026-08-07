6bdd4d9f6cb4446bb48520593e23ae028cf90a32
sun-mota <sun-mota@users.noreply.github.com>
2026-08-07 17:07:24 +0000

    Remove preview for PR 1574 🛬

--- pr-preview/pr-1574/dropdown/getting-started.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable jsdoc/require-jsdoc, no-magic-numbers, no-param-reassign */
-
-import { AuroDropdown } from '../src/auro-dropdown.js';
-AuroDropdown.register();
-AuroDropdown.register('custom-dropdown');
-
-export function initExamples() {
-  // javascript example function calls to be added here upon creation to test examples
-}
