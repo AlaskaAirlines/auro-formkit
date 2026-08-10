d093142a2ff07992e2e09eac36d8239d43488922
jason-capsule42 <jason-capsule42@users.noreply.github.com>
2026-08-10 21:58:13 +0000

    Deploy preview for PR 1583 🛫

---
+++ pr-preview/pr-1583/select/getting-started.js
@@ -0,0 +1,9 @@
+/* eslint-disable jsdoc/require-jsdoc */
+
+import { AuroSelect } from '../src/auro-select.js';
+import '../../menu/src/registered.js';
+
+AuroSelect.register();
+AuroSelect.register('custom-select');
+
+export function initExamples() {}
