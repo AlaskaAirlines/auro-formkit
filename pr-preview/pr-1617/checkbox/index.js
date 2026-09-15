473a2cd8c9a5a8856842ea5ae67193737a3a6cfb
jordanjones243 <jordanjones243@users.noreply.github.com>
2026-09-15 16:22:22 +0000

    Remove preview for PR 1617 🛬

--- pr-preview/pr-1617/checkbox/index.js
+++
@@ -1,8 +0,0 @@
-import { AuroCheckbox } from '../src/auro-checkbox.js';
-import { AuroCheckboxGroup } from '../src/auro-checkbox-group.js';
-
-AuroCheckbox.register(); // registering to `auro-checkbox`
-AuroCheckboxGroup.register(); // registering to `auro-checkbox-group`
-
-AuroCheckbox.register('custom-checkbox');
-AuroCheckboxGroup.register('custom-checkbox-group');
