5cb79bab8d0c3e04d3620a80310998338ff4f6f3
jason-capsule42 <jason-capsule42@users.noreply.github.com>
2026-09-24 21:13:35 +0000

    Remove preview for PR 1619 🛬

--- pr-preview/pr-1619/input/index.js
+++
@@ -1,8 +0,0 @@
-import { AuroInput } from '../src/auro-input.js';
-
-AuroInput.register();
-AuroInput.register('custom-input');
-
-export function initExamples(initCount) {
-  initCount = initCount || 0;
-}
